a253d7953fbca0f3dc9921fd2fde74c907a0e066
krandles <krandles@gmail.com>
2018-01-19 11:05:42 -0500

    done

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,10 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  validates :super_name, uniqueness: true
+  accepts_nested_attributes_for :superpower
+
+  def self.search(search)
+    joins(:superpower)
+    .where("superpowers.name LIKE ?", "%#{search}%")
+  end
 end
