c417cfdde62d4781f770d24a1e97e5e1fce5213b
github username <github email address>
2018-01-19 11:07:33 -0500

    Done

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,7 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+
+  validates :name, presence: true
+  validates :super_name, presence: true
+  validates :super_name, uniqueness: true
 end
