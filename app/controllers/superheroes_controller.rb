7dcf29ecfdda747eef8b659de2454c813c3f8959
github username <github email address>
2018-01-19 11:09:34 -0500

    Done.

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,47 @@
 class SuperheroesController < ApplicationController
+
   def index
     @superheroes = Superhero.all
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+    @all_superpowers = @superpowers.collect {|s| [s.name, s.id]}
+  end
+
+  def create
+    #byebug
+    @superhero = Superhero.create(superhero_params)
+
+    redirect_to @superhero
+  end
+
+  def edit
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def update
+    @superhero = Superhero.find(params[:id])
+    @superhero.update(superhero_params)
+
+    redirect_to @superhero
+  end
+
+  def destroy
+    @superhero = Superhero.find(params[:id])
+    @superhero.destroy
+
+    redirect_to superheroes_path
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :superpower_id)
   end
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,43 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def new
+    @superpower = Superpower.new
+  end
+
+  def create
+    #byebug
+    @superpower = Superpower.create(superpower_params)
+      redirect_to @superpower
+    end
+
+  def edit
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def update
+    @superpower = Superpower.find(params[:id])
+    @superpower.update(superpower_params)
+
+    redirect_to @superpower
+  end
+
+  def destroy
+    @superpower = Superpower.find(params[:id])
+    @superpower.destroy
+
+    redirect_to superpowers_path
+  end
+
+  private
+
+  def superpower_params
+    params.require(:superpower).permit(:name)
+  end
+
 end
