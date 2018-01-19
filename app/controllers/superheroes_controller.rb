3542c30915e140d46e655c604b2baef93ec97071
tanelam <tania.e.aparicio@gmail.com>
2018-01-19 11:09:39 -0500

    SUPERBEEF

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,44 @@
 class SuperheroesController < ApplicationController
+
   def index
     @superheroes = Superhero.all
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers =Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+    redirect_to superhero_path(@superhero)
+  end
+
+  def show
+    @superhero = Superhero.find_by(id: params[:id])
+    # @supepowers = @superhero.superpower_id
+  end
+
+  def edit
+    @superhero = Superhero.find_by(id: params[:id])
+     @superpowers = Superpower.all
+  end
+
+  def update
+    @superhero = Superhero.find(params[:id])
+    @superhero.update(superhero_params)
+    redirect_to superhero_path(@superhero)
+  end
+
+  def destroy
+    @superheroe = Superhero.find(params[:id])
+    @superheroe.destroy
+    redirect_to superheroes_path
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
   end
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,39 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  def new
+    @superpower = Superpower.new
+  end
+
+  def create
+    @superpower = Superpower.create(superpower_params)
+    redirect_to superpower_path(@superpower)
+  end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def edit
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def update
+    @superpower = Superpower.find(params[:id])
+    @superpower.update(superpower_params)
+    redirect_to superpower_path(@superpower)
+  end
+
+  def destroy
+    @superpower = Superpower.find(params[:id])
+    @superpower.destroy
+    redirect_to superpowers_path
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superpower).permit(:name, :description)
+  end
 end
